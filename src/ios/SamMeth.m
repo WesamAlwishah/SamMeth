617757598ab26f70d8b3c547f422e447c781a73f
unknown <wesamalwisha7@outlook.com>
2019-12-05 00:00:57 +0300

    initial commit

---
+++ src/ios/SamMeth.m
@@ -0,0 +1,28 @@
+/********* SamMeth.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface SamMeth : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation SamMeth
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
